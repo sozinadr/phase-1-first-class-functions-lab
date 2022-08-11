3a2126c6ff57bd2e8a47e52eca8cca0e2ccd0cc5
sozi <sozyarnadir@gmail.com>
2022-08-12 01:22:20 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => [drivers[0], drivers[1]];
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastIndex = drivers.length - 1;
+    return [drivers[lastIndex - 1], drivers[lastIndex]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(n) {
+        return num * n;
+    };
+}
+
+const fare = createFareMultiplier();
+
+const fareDoubler = (fare) => fare * 2;
+const fareTripler = (fare) => fare * 3;
+
+function selectDifferentDrivers(arrayOfDrivers, cb) {
+    return cb(arrayOfDrivers);
+}
